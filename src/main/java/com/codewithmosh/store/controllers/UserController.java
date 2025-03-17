86361605cb2e793eca323a823b87bd48ea5fe6c3
Mosh Hamedani <programmingwithmosh@gmail.com>
2025-03-17 11:27:07 -0400

    Extracting request body

--- src/main/java/com/codewithmosh/store/controllers/UserController.java
+++ src/main/java/com/codewithmosh/store/controllers/UserController.java
@@ -39,4 +39,9 @@ public class UserController {
 
         return ResponseEntity.ok(userMapper.toDto(user));
     }
+
+    @PostMapping
+    public UserDto createUser(@RequestBody UserDto data) {
+        return data;
+    }
 }
