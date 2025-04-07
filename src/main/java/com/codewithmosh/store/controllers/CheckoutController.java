a7ede2018687f45a72ba255a11155d6b235ce114
Mosh Hamedani <programmingwithmosh@gmail.com>
2025-04-07 11:32:16 -0400

    Updating order status

--- src/main/java/com/codewithmosh/store/controllers/CheckoutController.java
+++ src/main/java/com/codewithmosh/store/controllers/CheckoutController.java
@@ -3,12 +3,15 @@ package com.codewithmosh.store.controllers;
 import com.codewithmosh.store.dtos.CheckoutRequest;
 import com.codewithmosh.store.dtos.CheckoutResponse;
 import com.codewithmosh.store.dtos.ErrorDto;
+import com.codewithmosh.store.entities.OrderStatus;
 import com.codewithmosh.store.exceptions.CartEmptyException;
 import com.codewithmosh.store.exceptions.CartNotFoundException;
 import com.codewithmosh.store.exceptions.PaymentException;
+import com.codewithmosh.store.repositories.OrderRepository;
 import com.codewithmosh.store.services.CheckoutService;
 import com.stripe.exception.SignatureVerificationException;
 import com.stripe.exception.StripeException;
+import com.stripe.model.PaymentIntent;
 import com.stripe.net.Webhook;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
@@ -23,6 +26,7 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/checkout")
 public class CheckoutController {
     private final CheckoutService checkoutService;
+    private final OrderRepository orderRepository;
 
     @Value("${stripe.webhookSecretKey}")
     private String webhookSecretKey;
@@ -45,7 +49,13 @@ public class CheckoutController {
 
             switch (event.getType()) {
                 case "payment_intent.succeeded" -> {
-                    // Update order status (PAID)
+                    var paymentIntent = (PaymentIntent) stripeObject;
+                    if (paymentIntent != null) {
+                        var orderId = paymentIntent.getMetadata().get("order_id");
+                        var order = orderRepository.findById(Long.valueOf(orderId)).orElseThrow();
+                        order.setStatus(OrderStatus.PAID);
+                        orderRepository.save(order);
+                    }
                 }
                 case "payment_intent.failed" -> {
                     // Update order status (FAILED)
