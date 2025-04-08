db4eaab5a134494790bbd4d113bb1703f037d682
Mosh Hamedani <programmingwithmosh@gmail.com>
2025-04-08 16:38:06 -0400

    Organizing code by feature

--- src/main/java/com/codewithmosh/store/payments/CheckoutService.java
+++ src/main/java/com/codewithmosh/store/payments/CheckoutService.java
@@ -1,13 +1,12 @@
-package com.codewithmosh.store.services;
+package com.codewithmosh.store.payments;
 
-import com.codewithmosh.store.dtos.CheckoutRequest;
-import com.codewithmosh.store.dtos.CheckoutResponse;
 import com.codewithmosh.store.entities.Order;
 import com.codewithmosh.store.exceptions.CartEmptyException;
 import com.codewithmosh.store.exceptions.CartNotFoundException;
-import com.codewithmosh.store.exceptions.PaymentException;
 import com.codewithmosh.store.repositories.CartRepository;
 import com.codewithmosh.store.repositories.OrderRepository;
+import com.codewithmosh.store.services.AuthService;
+import com.codewithmosh.store.services.CartService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
