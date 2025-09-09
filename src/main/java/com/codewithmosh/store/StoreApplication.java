62199249aab9916bc73b6a0e58a684f0e73166f7
Thorsten Claus <thorstenclaus@web.de>
2025-09-09 11:32:43 +0200

    Adding sout for DSatasource URL

--- src/main/java/com/codewithmosh/store/StoreApplication.java
+++ src/main/java/com/codewithmosh/store/StoreApplication.java
@@ -1,12 +1,23 @@
 package com.codewithmosh.store;
 
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class StoreApplication {
 
+    @Value("${SPRING_DATASOURCE_URL}")
+    private String databaseUrl;
+
     public static void main(String[] args) {
         SpringApplication.run(StoreApplication.class, args);
     }
+
+    @PostConstruct
+    public void init() {
+        System.setProperty("spring.datasource.url: ", databaseUrl);
+    }
+
 }
